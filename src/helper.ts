c31ff2e57c7583d9ebec84026b9d2443bd0382eb
zhyzhang <22112020131@m.fudan.edu.cn>
2025-05-11 12:35:39 +0800

    Deleted DEBUG logic

--- src/helper.ts
+++ src/helper.ts
@@ -36,7 +36,6 @@ export default class Helper {
 
   getEditor() {
     const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
-    console.log('mdView: '+mdView); // DEBUG
     if (mdView) {
       return mdView.editor;
     } else {
